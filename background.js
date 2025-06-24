0501430e8562529c8d5483665a4773a16b966438
RoumaisaTanveer <ahmashade13@gmail.com>
2025-06-24 08:41:40 +0500

    Final!

--- background.js
+++
@@ -1,1 +0,0 @@
-console.log("GlassPen background running...");
--- popup.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('activate').addEventListener('click', async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: () => {
-      document.dispatchEvent(new CustomEvent('glasspen-activate'));
-    }
-  });
-});
